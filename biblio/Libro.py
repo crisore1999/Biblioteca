0fd74383da306f395ec8c6d4efa4e7d7c556187a
crisore99 <c.orellanalindo@campus.unimib.it>
2022-05-10 16:09:39 +0200

    add method toString() to the class Libro

--- biblio/Libro.py
+++ biblio/Libro.py
@@ -2,3 +2,6 @@ class  Libro:
     def __init__(self, titolo, autore):
         self.titolo = titolo
         self.autore = autore
+
+    def toString(self):
+        print("Autore: {}, Titolo: {}").format(self.autore, self.titolo)
