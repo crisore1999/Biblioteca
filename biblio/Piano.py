0e1486ceb256dbc33160c07cf704e09d7bf58704
crisore99 <c.orellanalindo@campus.unimib.it>
2022-05-11 17:45:07 +0200

    add methods addLibro and toString

--- biblio/Piano.py
+++ biblio/Piano.py
@@ -5,3 +5,13 @@ class Piano:
         for x in range(30):
             self.lista_scaffali.append(Scaffale())
 
+    def addLibro(self, scaffale, ripiano, titolo, autore):
+        add_libro = Libro(titolo, autore)
+        self.lista_scaffali[scaffale - 1].addLibro(ripiano, add_libro)
+
+    def toString(self):
+        stringa = "("
+        for x in self.lista_scaffali:
+            stringa += str(x)
+        stringa += ")"
+        return stringa
