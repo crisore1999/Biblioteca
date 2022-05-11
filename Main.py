f62671da0db3eb9d54aa69e701298678386a8765
crisore99 <c.orellanalindo@campus.unimib.it>
2022-05-11 17:43:58 +0200

    add objects biblioteca, piano, scaffale, ripiano, libro and test

--- Main.py
+++ Main.py
@@ -1,6 +1,14 @@
 from biblio.Biblioteca import Biblioteca
+from biblio.Piano import Piano
+from biblio.Scaffale import Scaffale
+from biblio.Ripiano import Ripiano
+from biblio.Libro import Libro
 print('Hello git')
 
 biblioteca = Biblioteca()
+piano = Piano()
+scaffale = Scaffale()
+ripiano = Ripiano()
+libro = Libro("Dante Alighieri", "Divina Commedia")
+biblioteca.contiene(piano, scaffale, ripiano)
 print("")
-#biblioteca.inserisciLibro("Dante Alighieri", "Divina Commedia")
