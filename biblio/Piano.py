0922a2ca6a674e8fe4be1567674678651f50b137
crisore99 <c.orellanalindo@campus.unimib.it>
2022-05-11 18:31:25 +0200

    update method addScaffale

--- biblio/Piano.py
+++ biblio/Piano.py
@@ -1,13 +1,13 @@
 from biblio.Scaffale import Scaffale
+from biblio.Libro import Libro
 class Piano:
     def __init__(self):
         self.lista_scaffali = list()
         for x in range(30):
             self.lista_scaffali.append(Scaffale())
 
-    def addLibro(self, scaffale, ripiano, titolo, autore):
-        add_libro = Libro(titolo, autore)
-        self.lista_scaffali[scaffale - 1].addLibro(ripiano, add_libro)
+    def addScaffale(self, scaffale, ripiano, libro):
+        self.lista_scaffali[scaffale - 1].addRipiano(ripiano, libro)
 
     def toString(self):
         stringa = "("
