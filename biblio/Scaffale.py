36ca67f3a18c564acc16c134d9f0dd56f303b769
crisore99 <c.orellanalindo@campus.unimib.it>
2022-05-11 17:47:31 +0200

    add methods addLibro and toString

--- biblio/Scaffale.py
+++ biblio/Scaffale.py
@@ -4,3 +4,14 @@ class Scaffale:
         self.lista_ripiani = dict({})
         for x in range(1, 7):
             self.lista_ripiani[("SC"+str(x))] = Ripiano()
+
+    def addLibro(self, ripiano, titolo, autore):
+        add_libro = Libro(titolo, autore)
+        self.lista_ripiani[ripiano-1].addLibro(add_libro)
+
+    def toString(self):
+        stringa = "("
+        for x in self.lista_ripiani:
+            stringa += str(x)
+        stringa += ")"
+        return stringa
