3705cd8143b22374805031400144d8414974aa98
crisore99 <c.orellanalindo@campus.unimib.it>
2022-05-11 17:42:07 +0200

    add methods contiene and addLibro

--- biblio/Biblioteca.py
+++ biblio/Biblioteca.py
@@ -1,15 +1,18 @@
 from biblio.Piano import Piano
+from biblio.Libro import Libro
 class Biblioteca:
     def __init__(self):
         piano1, piano2, piano3 = Piano(), Piano(), Piano()
         self.lista_piani = (piano1, piano2, piano3)
 
     def contiene(self, piano, scaffale, ripiano):
-        if ripiano[scaffale][piano] is not None:
-            print("C'è almeno un libro nel ripiano {}").format(ripiano)
-            ripiano.libri.toString()
+        if ripiano.libri is not None:
+            print("C'è almeno un libro nel piano {}, scaffale {}, ripiano {}\n".format(piano.toString(), scaffale.toString(), ripiano.toString()))
+
         else:
-            print("Non è presente nessun libro nel ripiano {}").format(ripiano)
+            print("Non è presente nessun libro nel piano {}, scaffale {}, ripiano {}\n".format(piano.toString(), scaffale.toString(), ripiano.toString()))
 
 
-
+    def addLibro(self, piano, scaffale, ripiano, titolo, autore):
+        add_libro = Libro(titolo, autore)
+        self.lista_piani[piano - 1].addLibro(scaffale, ripiano, add_libro)
