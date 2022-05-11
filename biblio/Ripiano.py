5315e1fc489b996f8931612506714a5cec301a72
crisore99 <c.orellanalindo@campus.unimib.it>
2022-05-11 17:46:11 +0200

    add methods addLibro and toString

--- biblio/Ripiano.py
+++ biblio/Ripiano.py
@@ -4,3 +4,14 @@ class Ripiano:
         self.libri = list()
         for x in range(10):
             self.libri.append(Libro)
+
+    def addLibro(self, titolo, autore):
+        add_libro = Libro(titolo, autore)
+        self.libri.append(add_libro)
+
+    def toString(self):
+        stringa = "("
+        for x in self.libri:
+            stringa += str(x)
+        stringa += ")"
+        return stringa
